b85cfcd792007f2261a24b56463747b312b7c7e6
Raidon Amakobe <Raidonmgmt@gmail.com>
2019-05-06 16:29:58 -0400

    Create 3 <Route />

--- src/App.js
+++ src/App.js
@@ -6,7 +6,11 @@ import { Route } from 'react-router-dom';
 const App = () => (
   <div>
     <Navigation />
+    <Route exact path = '/' component = {Home} />
+    <Route exact path = '/about' component = {About} />
+    <Route exact path = '/contact' component = {Contact} />
   </div>
+
 );
 
 export default App;
